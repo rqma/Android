00d571afc1fc4bad6f55242b68d4d657b9acf4bc
rqma <rq.ma@outlook.com>
2018-10-12 17:42:16 +0800

    commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @Auther: Adi
+ * @Date: 2018/10/12 17:40
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello ggg");
+    }
+}
